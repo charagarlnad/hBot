ffa938dcfc7b2c337ac4763dd2f912a5413d117b
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-03-27 19:29:10 -0400

    anger has begun

--- src/modules/commands/general/help2.rb
+++ src/modules/commands/general/help2.rb
@@ -3,8 +3,8 @@ module Bot::DiscordCommands
       extend Discordrb::Commands::CommandContainer
       command :help2 do |event|
         event.channel.send_embed do |embed|
-            Dir["src/modules/commands/**"].each do |base|
-            commandarr = Dir["src/modules/commands/owner/*.rb"].map { |prepend| "#{event.bot.prefix}#{prepend}" }
+          Dir["src/modules/commands/**"].each do |base|
+            commandarr = Dir[base].map { |prepend| "#{event.bot.prefix}#{prepend}" }
             embed.add_field(name: base, value: commandarr.join("\n").gsub(base, ''))
           end
           embed.title = '**Imgbot (click me to invite me to your server!)**'
