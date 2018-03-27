835901c5040c135cc6a509ba4407f90ec5f079c1
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-03-27 19:24:45 -0400

    j

--- src/modules/commands/general/help2.rb
+++ src/modules/commands/general/help2.rb
@@ -3,9 +3,9 @@ module Bot::DiscordCommands
       extend Discordrb::Commands::CommandContainer
       command :help2 do |event|
         event.channel.send_embed do |embed|
-          Dir["src/modules/commands/**"].each do |base|
-            commandarr = Dir[base].map { |prepend| "#{event.bot.prefix}#{prepend}" }
-            embed.add_field(name: base, value: commandarr.join("\n"))
+            Dir["src/modules/commands/**"].each do |base|
+            commandarr = Dir["src/modules/commands/owner/*.rb"].map { |prepend| "#{event.bot.prefix}#{prepend}" }
+            embed.add_field(name: base, value: commandarr.join("\n").gsub(base, ''))
           end
           embed.title = '**Imgbot (click me to invite me to your server!)**'
           embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: event.bot.profile.avatar_url)
