7adc8dde4a3433c4a29f44c222dbf92846032c1e
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-03-27 20:09:14 -0400

    oops wrong brackets

--- src/modules/commands/general/help2.rb
+++ src/modules/commands/general/help2.rb
@@ -4,7 +4,7 @@ module Bot::DiscordCommands
       command :help2 do |event|
         event.channel.send_embed do |embed|
           Dir["src/modules/commands/**"].each do |base|
-            commandarr = Dir.children[base].map { |prepend| "#{event.bot.prefix}#{prepend}" } #ruby 2.3 or above boys
+            commandarr = Dir.children(base).map { |prepend| "#{event.bot.prefix}#{prepend}" } #ruby 2.3 or above boys
             embed.add_field(name: base, value: commandarr.join("\n").gsub('.rb', ''))
           end
           embed.title = '**Imgbot (click me to invite me to your server!)**'
