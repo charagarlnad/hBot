55044910779c12873a8b88738d9a16e7500334b9
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-03-31 18:34:12 -0400

    cache clearing

---
+++ src/modules/commands/owner/clearcache.rb
@@ -0,0 +1,11 @@
+module Bot::DiscordCommands
+  module Music
+    extend Discordrb::Commands::CommandContainer
+    command :clearcache do |event|
+      if event.user.id == 123927345307451392
+        Pathname.new('data/musiccache/').children.each { |p| p.unlink }
+        event.respond 'Cache cleared!'
+      end
+    end
+  end
+end
