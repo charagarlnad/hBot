05a09c153710896251b702433348f3ecf7ea3f61
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-04-15 02:21:44 -0400

    Add delete_user_reaction to monkey patch

--- src/modules/react_monkeypatch.rb
+++ src/modules/react_monkeypatch.rb
@@ -11,6 +11,15 @@ module Discordrb::API::Channel
     )
     sleep(0.25)
   end
+  def delete_user_reaction(token, channel_id, message_id, emoji, user_id)
+    emoji = URI.encode(emoji) unless emoji.ascii_only?
+    Discordrb::API.raw_request(
+      :delete,
+      ["#{Discordrb::API.api_base}/channels/#{channel_id}/messages/#{message_id}/reactions/#{emoji}/#{user_id}",
+      Authorization: token]
+    )
+    sleep(0.25)
+  end
 end
 # https://github.com/meew0/discordrb/blob/cb074e0f80c078c09ca432893c1601d28bf78adb/lib/discordrb/api/channel.rb#L142
 # https://github.com/meew0/discordrb/blob/ef88d4ed27fc2520c1db3fcb21897f60e7d39bc3/lib/discordrb/api.rb#L90
