c9a99c40343311286cf24069d459242f8c9d246c
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-04-19 16:57:30 -0400

    Add blacklist command

---
+++ src/modules/commands/owner/blacklist.rb
@@ -0,0 +1,14 @@
+module Bot::DiscordCommands
+  module Owner
+    extend Discordrb::Commands::CommandContainer
+    if File.file?("data/blacklist.txt")
+      File.read("data/blacklist.txt").each_line { |user| Bot::BOT.ignore_user(user) }
+    end
+    command(:blacklist, type: :Owner, description: 'Blacklist a user from the bot.') do |event, mention|
+      if event.user.id == 123927345307451392
+        event.bot.ignore_user(event.bot.parse_mention(mention).id)
+        File.open('data/blacklist.txt', 'w') {|f| f.write(event.bot.ignored_ids.to_a.join("\n")) }
+      end
+    end
+  end
+end
---
+++ src/modules/patch_bot.rb
@@ -0,0 +1,5 @@
+module Discordrb
+  class Bot
+    attr_reader :ignored_ids
+  end
+end
