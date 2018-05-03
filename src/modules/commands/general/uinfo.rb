af98b19f60b6846fa372f7518f409c096d342d43
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-05-03 02:23:02 -0400

    Make uinfo take a optional mention.

--- src/modules/commands/general/uinfo.rb
+++ src/modules/commands/general/uinfo.rb
@@ -1,16 +1,22 @@
 module Bot::DiscordCommands
   module General
     extend Discordrb::Commands::CommandContainer
-    command(:uinfo, type: :General, description: 'Get a list of statistics about the user that runs the command.') do |event|
-      # Allow mentions of other users
+    command(:uinfo, type: :General, description: 'Get a list of statistics about the user that runs the command.') do |event, mention|
+      # Allow just typing name without a mention
+      user = 
+        if mention&.match?(/<@!?\d{10,}>/)
+          event.bot.parse_mention(mention).on(event.server)
+        else
+          event.user
+        end
       event.channel.send_embed do |embed|
         embed.title = 'User Info'
-        embed.add_field(name: 'Name#Discrim', value: "#{event.user.name}\##{event.user.discrim}", inline: true)
-        embed.add_field(name: 'Status', value: event.user.status, inline: true)
-        embed.add_field(name: 'Currently Playing', value: event.user.game, inline: true)
-        embed.add_field(name: 'Created at', value: "#{event.user.creation_time.getutc.asctime} UTC", inline: true)
-        embed.add_field(name: 'Joined server at', value: "#{event.user.joined_at.getutc.asctime} UTC", inline: true)
-        embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: event.user.avatar_url)
+        embed.add_field(name: 'Name#Discrim', value: "#{user.name}\##{user.discrim}", inline: true)
+        embed.add_field(name: 'Status', value: user.status, inline: true)
+        embed.add_field(name: 'Currently Playing', value: user.game ? user.game : 'N/A', inline: true)
+        embed.add_field(name: 'Created at', value: "#{user.creation_time.getutc.asctime} UTC", inline: true)
+        embed.add_field(name: 'Joined server at', value: "#{user.joined_at.getutc.asctime} UTC", inline: true)
+        embed.thumbnail = Discordrb::Webhooks::EmbedThumbnail.new(url: user.avatar_url)
       end
     end
   end
