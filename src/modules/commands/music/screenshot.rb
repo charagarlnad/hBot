886d012efd5a3c19dc5ee0cb160f78e67082eff2
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-05-03 02:00:52 -0400

    Speed up screenshot by never saving the screenshot to the filesystem.

--- src/modules/commands/music/screenshot.rb
+++ src/modules/commands/music/screenshot.rb
@@ -2,8 +2,8 @@ module Bot::DiscordCommands
   module Music
     extend Discordrb::Commands::CommandContainer
     command(:screenshot, requirements: [:in_voice, :playing], type: :Music, description: 'Send a screenshot of the current video.') do |event|
-      system("ffmpeg -loglevel panic -y -ss #{event.voice.stream_time.to_i + Bot.masterqueue[event.server.id].first[:skipped_time]} -i #{Bot.masterqueue[event.server.id].first[:location]} -vframes 1 data/musiccache/screenshot.png")
-      emb = event.channel.send_file File.new('data/musiccache/screenshot.png')
+      file = IO.popen(['ffmpeg', '-loglevel', 'panic', '-ss', (event.voice.stream_time.to_i + Bot.masterqueue[event.server.id].first[:skipped_time]).to_s, '-i', Bot.masterqueue[event.server.id].first[:location], '-f', 'image2', 'pipe:']).read
+      emb = event.channel.send_file BinaryImage.new(file, "#{event.message.id}.png")
 
       sleep(Bot.embedtimeout)
       emb.delete
