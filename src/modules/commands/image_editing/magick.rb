b077dfee1b23dd6decfa28867bbc8fb5c587f7c5
charagarlnad <27380203+charagarlnad@users.noreply.github.com>
2018-05-02 23:22:21 -0400

    Improve magick commands.

--- src/modules/commands/image_editing/magick.rb
+++ src/modules/commands/image_editing/magick.rb
@@ -1,8 +1,12 @@
 module Bot::DiscordCommands
   module ImageEditing
     extend Discordrb::Commands::CommandContainer
-    command(:magick, type: :'Image Editing') do |event|
-      append_image = Magick::Image.from_blob(event.image_source).first.scale(800, 800).liquid_rescale(400, 400, 1, 0).liquid_rescale(1000, 1000, 2, 0)
+    command(:magick, type: :'Image Editing', description: 'Apply Magik to a [**image**].') do |event|
+      append_image = Magick::Image.from_blob(event.image_source).first
+
+      # https://rmagick.github.io/image2.html#liquid_rescale
+      append_image = append_image.liquid_rescale(append_image.columns * 0.5, append_image.rows * 0.5, 1)
+      append_image = append_image.liquid_rescale(append_image.columns * 1.5, append_image.rows * 1.5, 2)
 
       event.channel.send_file BinaryImage.new(append_image.to_blob, "#{event.message.id}.png")
 
